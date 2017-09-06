0c724966100f6769214981299f745db3a4e80b14
Dan Carpenter <daniel.carpenter01@gmail.com>
2017-09-06 00:57:39 -0400

    Handle empty values in the contact model

--- lib/ct_gov/contact.rb
+++ lib/ct_gov/contact.rb
@@ -1,24 +1,24 @@
 module CtGov
   class Contact
-    
+
     def initialize(raw_contact)
       @raw_contact = raw_contact
     end
-    
+
     def name
-      @raw_contact['last_name']
+      @raw_contact['last_name'].to_s if @raw_contact['last_name']
     end
-    
+
     def phone
-      @raw_contact['phone']
+      @raw_contact['phone'].to_s if @raw_contact['phone']
     end
-    
+
     def phone_extension
-      @raw_contact['phone_ext']
+      @raw_contact['phone_ext'].to_s if @raw_contact['phone_ext']
     end
-    
+
     def email
-      @raw_contact['email']
+      @raw_contact['email'].to_s if @raw_contact['email']
     end
   end
 end
